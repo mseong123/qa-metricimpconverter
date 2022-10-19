e5b8ebb9b7713c91a4fd2db1dfb989ba0d6ca1f8
Mseong <mseong123@hotmail.com>
2022-10-19 16:46:03 +0800

    done logic

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,24 +2,74 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
     let result;
+    if (Array.isArray(input.match(/\d+[+-/*]*/g))) {
+      result=eval(input.match(/\d+[+-/*]*/g).join(''))
+    }
     
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
-    
+    let result
+    if (Array.isArray(input.match(/[a-zA-Z]+/))) {
+      result=input.match(/[a-zA-Z]+/).join()
+    }
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
+    initUnit=initUnit.toLowerCase();
     let result;
-    
+    switch (initUnit){
+      case 'gal':
+        result='L';
+        break;
+      case 'l':
+        result='gal';
+        break;
+      case 'km':
+        result='mi';
+        break;
+      case 'mi':
+        result='km';
+        break;
+      case 'kg':
+        result='lbs';
+        break;
+      case 'lbs':
+        result='kg';
+        break;
+      default:
+        break;
+    }
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
+
+    switch (unit){
+      case 'lbs':
+        result='pounds';
+        break;
+      case 'kg':
+        result='kilograms';
+        break;
+      case 'gal':
+        result='gallons';
+        break;
+      case 'L':
+        result='liters';
+        break;
+      case 'km':
+        result='kilometers';
+        break;
+      case 'mi':
+        result='miles';
+        break;
+      default:
+        break;
+    }
     
     return result;
   };
@@ -29,13 +79,40 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
+
+    switch (initUnit){
+      case 'lbs':
+        result=initNum*lbsToKg;
+        break;
+      case 'kg':
+        result=initNum/lbsToKg;
+        break;
+      case 'gal':
+        result=initNum*galToL;
+        break;
+      case 'L':
+        result=initNum/galToL;
+        break;
+      case 'km':
+        result=initNum/miToKm;
+        break;
+      case 'mi':
+        result=initNum*miToKm;
+        break;
+      default:
+        break;
+    }
+
     
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
     
+    let result = initNum+' '+this.spellOutUnit(initUnit)+' converts to '+returnNum+' '+this.spellOutUnit('km')
+    
+    
+
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,19 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
-
-};
+  app.get('/api/convert',function(req,res,next) {
+    let input=req.query.input;
+    let initNum=convertHandler.getNum(input)
+    let initUnit=convertHandler.getUnit(input)
+    
+    res.send(
+      {initNum:initNum,
+        initUnit:initUnit,
+        returnNum:convertHandler.convert(initNum,initUnit),
+        returnUnit:convertHandler.getReturnUnit(initUnit),
+        string:convertHandler.getString(initNum,initUnit,convertHandler.convert(initNum,initUnit),convertHandler.getReturnUnit(initUnit))
+        }
+      
+    )
+    })
+  };
